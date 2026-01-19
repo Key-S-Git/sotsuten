c2d51781d3dc35d1280be8fe727a118addb6e454
Key-S <0rp32554022302d@gmail.com>
2026-01-19 16:42:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,3 +53,13 @@
 				s), i
 		}
 })();
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    // 50pxスクロールしたらクラスを付与
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
